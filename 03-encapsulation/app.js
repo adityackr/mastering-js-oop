5fddff248ea4d10a58d3bb3eb13d6abe54337f75
adityackr <aditya09me@gmail.com>
2022-08-30 20:55:33 +0600

    ongoing: 03-encapsulation

--- 03-encapsulation/app.js
+++ 03-encapsulation/app.js
@@ -2,15 +2,30 @@
 
 // * Convention based approach
 
-function TheatreSeats() {
-	this._seats = [];
-}
+// function TheatreSeats() {
+// 	this._seats = [];
+// }
 
-TheatreSeats.prototype.placePerson = function (person) {
-	this._seats.push(person);
-};
+// TheatreSeats.prototype.placePerson = function (person) {
+// 	this._seats.push(person);
+// };
 
-const theatreSeats = new TheatreSeats();
-theatreSeats.placePerson({ name: 'Aditya', surname: 'Chakraborty' });
+// const theatreSeats = new TheatreSeats();
+// theatreSeats.placePerson({ name: 'Aditya', surname: 'Chakraborty' });
 
-console.log(theatreSeats);
+// console.log(theatreSeats);
+
+// * Privacy levels using closure
+
+// function TheatreSeats() {
+// 	const seats = [];
+
+// 	this.placePerson = function (person) {
+// 		seats.push(person);
+// 	};
+// }
+
+// const theatreSeats = new TheatreSeats();
+// theatreSeats.placePerson({ name: 'Aditya', surname: 'Chakraborty' });
+
+// console.log(theatreSeats);
